a315a372d5f42516086a3c2fa58d3c7de022bbbb
Eric Miao <ericxmiao@gmail.com>
2024-05-04 19:16:12 -0400

    Gemini initial code

--- API-comms/main.py
+++ API-comms/main.py
@@ -1,8 +1,28 @@
-# pip library
-import argparse
+# pip dependencies: argparse, google-generativeai, python-dotenv
+import os#, argparse
+import google.generativeai as genai
+from dotenv import load_dotenv, dotenv_values
+
+class GeminiAI:
+    def __init__(self):
+        genai.configure(api_key=os.getenv("API_KEY"))
+        # text only, gemini-pro-vision: text and image
+        self.model = genai.GenerativeModel('gemini-pro')
+
+    def get_response_default(self, query:str) -> str:
+        response = self.model.generate_content(query)
+        print(response.text)
+        return response.text
+
+
+def prep():
+    load_dotenv()
+
+def main():
+    prep()
+    aiobj = GeminiAI()
+    # aiobj.get_response_default("Who was elected the president of the US in 2012")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--action', '-a', required=False, help="Action")
-    arguments = parser.parse_args()
-    print('Argument for action: ' + arguments.action)
+    main()
+
