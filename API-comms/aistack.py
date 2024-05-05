2ff8f8576f6c39c8a21ef8a3214ba899d4c56377
Eric Miao <ericxmiao@gmail.com>
2024-05-05 13:38:58 -0400

    Separated AI to its own object file

---
+++ API-comms/aistack.py
@@ -0,0 +1,14 @@
+"""AI Stack: This file hosts the main objects for each compatible generative AI tool
+"""
+import google.generativeai as genai
+
+class GeminiAI:
+    def __init__(self, api_key):
+        genai.configure(api_key=api_key)
+        # text only, gemini-pro-vision: text and image
+        self.model = genai.GenerativeModel('gemini-pro')
+
+    def get_response_default(self, query:str) -> str:
+        response = self.model.generate_content(query)
+        print(response.text)
+        return response.text
--- API-comms/main.py
+++ API-comms/main.py
@@ -1,28 +1,15 @@
 # pip dependencies: argparse, google-generativeai, python-dotenv
 import os#, argparse
-import google.generativeai as genai
 from dotenv import load_dotenv, dotenv_values
-
-class GeminiAI:
-    def __init__(self):
-        genai.configure(api_key=os.getenv("API_KEY"))
-        # text only, gemini-pro-vision: text and image
-        self.model = genai.GenerativeModel('gemini-pro')
-
-    def get_response_default(self, query:str) -> str:
-        response = self.model.generate_content(query)
-        print(response.text)
-        return response.text
-
+from aistack import GeminiAI
 
 def prep():
     load_dotenv()
 
 def main():
     prep()
-    aiobj = GeminiAI()
+    aiobj = GeminiAI(os.getenv("API_KEY"))
     # aiobj.get_response_default("Who was elected the president of the US in 2012")
 
 if __name__ == "__main__":
     main()
-
