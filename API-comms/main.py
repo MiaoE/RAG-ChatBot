b148ffe28743c53492f2901a9bfbd0e99d052825
Eric Miao <ericxmiao@gmail.com>
2024-05-07 13:29:04 -0400

    Update main.py

--- API-comms/main.py
+++ API-comms/main.py
@@ -1,14 +1,14 @@
 # pip dependencies: python-dotenv
 import os
 from dotenv import load_dotenv, dotenv_values
-from aistack import GeminiAI
+from aicluster import GeminiAI
 
 def prep():
     load_dotenv()
 
 def main():
     prep()
-    aiobj = GeminiAI(os.getenv("API_KEY"))
+    aiobj = GeminiAI(os.getenv("GEMINI_API_KEY"))
     # aiobj.get_response_default("Who was elected the president of the US in 2012")
 
 if __name__ == "__main__":
