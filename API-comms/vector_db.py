fe976ae09fe4a5ae3d46da40bee7103e3c4229df
Eric Miao <ericxmiao@gmail.com>
2024-05-07 13:33:17 -0400

    Create vector_db.py

---
+++ API-comms/vector_db.py
@@ -0,0 +1,16 @@
+from pinecone import Pinecone, ServerlessSpec
+
+class VectorDB:
+    def __init__(self, api_key):
+        self.pc = Pinecone(api_key=api_key)
+    
+    def create_index(self):
+        self.pc.create_index(
+            name="quickstart",
+            dimension=8, # Replace with your model dimensions
+            metric="euclidean", # Replace with your model metric
+            spec=ServerlessSpec(
+                cloud="aws",
+                region="us-east-1"
+            ) 
+        )
