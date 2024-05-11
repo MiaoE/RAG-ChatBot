e903ddb8305da47d75ceb6b1526e7ed69beeb1ff
WlliamZhao <71152785+WilliamZhao1@users.noreply.github.com>
2024-05-11 16:30:21 -0400

    Set up embedding model and FAISS Database

--- API-comms/main.py
+++ API-comms/main.py
@@ -5,6 +5,11 @@ from aicluster import GeminiAI
 
 from langchain_community.document_loaders import HuggingFaceDatasetLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
+from transformers import AutoTokenizer, AutoModel
+from sentence_transformers import SentenceTransformer
 
 # Initialize dataset
 dataset_name = "databricks/databricks-dolly-15k"
@@ -22,15 +27,33 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=15
 
 docs = text_splitter.split_documents(data)
 
+# Define embedding model
+modelPath = "sentence-transformers/all-MiniLM-l6-v2"
+model_kwargs = {'device':'cpu'}
+encode_kwargs = {'normalize_embeddings': False}
+
+embeddings = HuggingFaceEmbeddings(
+    model_name=modelPath,     # Provide the pre-trained model's path
+    model_kwargs=model_kwargs, # Pass the model configuration options
+    encode_kwargs=encode_kwargs # Pass the encoding options
+)
+
+# Load into database (FAISS)
+db = FAISS.from_documents(docs, embeddings)
+
+question = "Why can camels survive for long without water?"
+searchDB = db.similarity_search(question)
+print(searchDB[0].page_content)
 
 
-def prep():
-    load_dotenv()
 
-def main():
-    prep()
-    aiobj = GeminiAI(os.getenv("GEMINI_API_KEY"))
-    # aiobj.get_response_default("Who was elected the president of the US in 2012")
+# def prep():
+#     load_dotenv()
 
-if __name__ == "__main__":
-    main()
+# def main():
+#     prep()
+#     aiobj = GeminiAI(os.getenv("GEMINI_API_KEY"))
+#     # aiobj.get_response_default("Who was elected the president of the US in 2012")
+
+# if __name__ == "__main__":
+#     main()
